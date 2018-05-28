af59a6e9d714d6b0e6ddf5847471ea12bdf32a7c
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-28 13:57:37 +0200

    page sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/signin', to: 'home#sign_in'
+  get '/signup', to: 'home#sign_up'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
