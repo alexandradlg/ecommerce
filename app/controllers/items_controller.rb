3b424291e4bb2e7f2bc5b34663f8818e5e369d56
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-29 18:58:55 +0200

    stripe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,31 +11,33 @@ class ItemsController < ApplicationController
 	end
 
 	def cart
+		@cart = Cart.find(params[:id])
 	end
 
 	def proceed_payment
-		@order = Order.find(params[:order_id])
+		@order = Cart.find(params[:id])
+    	@amount = @order.items.sum(:price)
+    	customer = Stripe::Customer.create(
+    	  :email => params[:stripeEmail],
+    	  :source  => params[:stripeToken]
+    	)
 
-      @amount = @order.price_cents
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
-      )
-    
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'Rails Stripe customer',
-        :currency    => 'eur'
-      )
+    	charge = Stripe::Charge.create(
+    	  :customer    => customer.id,
+    	  :amount      => @amount,
+    	  :description => 'Rails Stripe customer',
+    	  :currency    => 'eur'
+    	)	
+    	Order.create(user_id: @order.user_id, order_total: @amount)
+    	redirect_to thankyou_path
+    	flash[:success] = "You are now attending this event"
 
-      @event.attendees << current_user
-      redirect_to @event
-      flash[:success] = "You are now attending this event"
-    
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to @event
+    	rescue Stripe::CardError => e
+    	  flash[:error] = e.message
+    	  redirect_to panier_path
+	end
+
+	def checkout_finish
 	end
 
 end
