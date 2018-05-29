b48f829172f9f9697f0dd1e2d56b53c364448b90
Quentin <berson.quentin@gmail.com>
2018-05-29 17:28:28 +0200

    ajout QB

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,14 +2,23 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation
 
-    ...rest of method...
-    mail(to: @user.email, subject: "Word", reply_to: 'dev@null.com') do |format|
+    @order = params[:order]
+    @user = @order.user
+
+    mail(to: @user.email, subject: "Order confirmation", reply_to: 'esportdigest@gmail.com') do |format|
       format.html { render 'order_mailer/order_confirmation'}
     end
 
   end
 
   def admin_order_confirmation
+
+    @order = params[:order]
+
+    mail(to: 'esportdigest@gmail.com', subject: "New Order", reply_to: 'esportdigest@gmail.com') do |format|
+      format.html { render 'order_mailer/admin_order_confirmation'}
+    end
+
   end
 
 end
