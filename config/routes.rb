e5c8c227aa9503327f70d1a5ca2124ea01229e4e
miguel <miguelsorinpro@gmail.com>
2018-05-29 11:13:27 +0200

    add_items

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/signin', to: 'home#sign_in'
   get '/signup', to: 'home#sign_up'
+  get 'items/index', to: 'items#index'
+  get 'items', to: 'items#index'
+  get 'items/:id', to: 'items#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
