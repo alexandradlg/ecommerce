a3556545bbc8415e89252914232a4a20e91e538c
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-29 18:13:54 +0200

    connecting stripe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,4 +13,29 @@ class ItemsController < ApplicationController
 	def cart
 	end
 
+	def proceed_payment
+		@order = Order.find(params[:order_id])
+
+      @amount = @order.price_cents
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+    
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'eur'
+      )
+
+      @event.attendees << current_user
+      redirect_to @event
+      flash[:success] = "You are now attending this event"
+    
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to @event
+	end
+
 end
