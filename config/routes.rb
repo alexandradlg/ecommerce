45574b0890b53048903211298a4e3b10fc629160
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-29 18:06:48 +0200

    cart

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'items/:id', to: 'items#show', as: 'show_item'
   get '/undefined', to: 'home#undefined'
   get '/show', to: 'home#show'
+  get '/panier', to: 'items#cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
