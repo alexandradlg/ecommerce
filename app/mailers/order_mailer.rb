f7e08ff1c742a8b35cc0513ac4a3e4058fd7d1cf
Alexandra DLG <adelagueronniere@gmail.com>
2018-05-30 09:18:13 +0200

    comment mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,15 +1,15 @@
 class OrderMailer < ApplicationMailer
 
-  def order_confirmation
+  # def order_confirmation
 
-    ...rest of method...
-    mail(to: @user.email, subject: "Word", reply_to: 'dev@null.com') do |format|
-      format.html { render 'order_mailer/order_confirmation'}
-    end
+  #   ...rest of method...
+  #   mail(to: @user.email, subject: "Word", reply_to: 'dev@null.com') do |format|
+  #     format.html { render 'order_mailer/order_confirmation'}
+  #   end
 
-  end
+  # end
 
-  def admin_order_confirmation
-  end
+  # def admin_order_confirmation
+  # end
 
 end
