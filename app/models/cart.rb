906c43d9e30e5940113564b5a5341d590bfaa8c8
LeoDeb <leo.deboin@gmail.com>
2018-05-30 09:33:21 +0200

    dur

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,16 +1,17 @@
 class Cart < ApplicationRecord
     belongs_to :user
-    has_many :carts_items
+    has_many :items
 
- def add_items(items_params)
+ 
+  def add_items(items_params)
 	current_item = carts_items.find_by(item_id: item_params[:item][:item_id])
 	if current_item
-		
-
-
 
 
 end
+
+
+
 
 
 end
